10a479f580965f0c0d63e17627b2dd7b40dc1b08
Robert <robert@robertlong.me>
2013-09-15 10:46:43 -0700

    Initial commit

---
+++ src/robot.py
@@ -0,0 +1,43 @@
+try:
+    import wpilib
+except ImportError:
+    import fake_wpilib as wpilib
+
+lstick = wpilib.Joystick(1)
+
+motor = wpilib.Victor(1)
+
+def CheckRestart():
+    if lstick.GetRawButton(10):
+        raise RuntimeError("Restart")
+
+class MyRobot(wpilib.SimpleRobot):
+    def Disabled(self):
+        while self.IsDisabled():
+            CheckRestart()
+            wpilib.Wait(0.01)
+
+    def Autonomous(self):
+        self.GetWatchdog().SetEnabled(False)
+        while self.IsAutonomous() and self.IsEnabled():
+            CheckRestart()
+            wpilib.Wait(0.01)
+
+    def OperatorControl(self):
+        dog = self.GetWatchdog()
+        dog.SetEnabled(True)
+        dog.SetExpiration(0.25)
+
+        while self.IsOperatorControl() and self.IsEnabled():
+            dog.Feed()
+            CheckRestart()
+
+            # Motor control
+            motor.Set(lstick.GetY())
+
+            wpilib.Wait(0.04)
+
+def run():
+    robot = MyRobot()
+    robot.StartCompetition()
+    return robot
